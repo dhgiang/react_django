832216e49a5a869a1e432b8215982bfdbf15a1c4
Duc Giang <ducgiang@Macintosh.local>
2019-03-31 16:14:18 -0400

    first commit maybe

---
+++ leadmanager/frontend/src/actions/leads.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { createMessage, returnErrors } from "./messages";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
+
+// GET_LEADS
+export const getLeads = () => dispatch => {
+  axios
+    .get("/api/leads/")
+    .then(res => {
+      dispatch({
+        type: GET_LEADS,
+        payload: res.data
+      });
+    })
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
+};
+
+export const deleteLead = id => dispatch => {
+  axios
+    .delete(`/api/leads/${id}/`)
+    .then(res => {
+      dispatch(createMessage({ deleteLead: "Lead deleted!" }));
+      dispatch({
+        type: DELETE_LEAD,
+        payload: id
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+export const addLead = lead => dispatch => {
+  axios
+    .post("/api/leads/", lead)
+    .then(res => {
+      dispatch(createMessage({ addLead: "Lead added!" }));
+      dispatch({
+        type: ADD_LEAD,
+        payload: res.data
+      });
+    })
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
+};
